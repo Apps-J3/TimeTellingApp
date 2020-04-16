73e34e62fe280722e72750bcb914c6849fbf06a1
mdari <mdariotis@charter.net>
2020-04-16 14:44:48 -0400

    maria was here pt. 2

--- core/src/com/timetelling/gameworld/GameRenderer.java
+++ core/src/com/timetelling/gameworld/GameRenderer.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Graphics;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.timetelling.game.TimeTellingGame;
@@ -20,6 +21,7 @@ public class GameRenderer {
     protected int width, height;
     protected TimeTellingGame game;
     private SimpleButton backButton;
+    private Texture background;
 
     public GameRenderer(GameWorld world, TimeTellingGame game) {
         this.game = game;
@@ -35,11 +37,17 @@ public class GameRenderer {
         backButton = new SimpleButton(20, height-20-height/20, width/10, height/20, "Home");
     }
 
+    public void create() {
+        batcher = new SpriteBatch();
+        background = new Texture("background.png");
+    }
+
     public void render() {
-        Gdx.gl.glClearColor(0, 0, 0, 1);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        //Gdx.gl.glClearColor(0, 0, 0, 1);
+        //Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         batcher.begin();
         backButton.draw(batcher);
+        batcher.draw(background,0,0);
         batcher.end();
     }
 
