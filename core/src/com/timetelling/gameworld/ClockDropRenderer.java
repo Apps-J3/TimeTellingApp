16827d10299d3f32e9539f8ce505cfd224797690
spferrante <spferrante@wpi.edu>
2020-05-04 15:33:48 -0400

    Clock Drop Game

--- core/src/com/timetelling/gameworld/ClockDropRenderer.java
+++ core/src/com/timetelling/gameworld/ClockDropRenderer.java
@@ -5,6 +5,7 @@ import com.timetelling.game.TimeTellingGame;
 import com.timetelling.gameobjects.Clock;
 import com.timetelling.gameobjects.ProgressBar;
 import com.timetelling.gameobjects.SimpleButton;
+import com.timetelling.helper.AssetLoader;
 import com.timetelling.screens.MovableClockScreen;
 
 public class ClockDropRenderer extends GameRenderer {
@@ -19,9 +20,11 @@ public class ClockDropRenderer extends GameRenderer {
     public ClockDropRenderer(GameWorld world, TimeTellingGame game) {
         super(world, game);
         this.world = (ClockDropWorld)world;
-        trashButton = new SimpleButton(width/5, width/4, width/5, width/4, "Trash");
-        turtleButton = new SimpleButton(3*width/5, width/4, width/5, width/4, "Turtle");
+        trashButton = new SimpleButton(width/5, width/4, width/5, width/8, "Trash");
+        turtleButton = new SimpleButton(3*width/5, width/4, width/5, width/8, "Turtle");
         bar = this.world.getBar();
+        font = AssetLoader.font;
+        clock = new Clock(this.world.getClockTime());
     }
 
     @Override
@@ -29,6 +32,8 @@ public class ClockDropRenderer extends GameRenderer {
         super.render();
         batcher.begin();
         drawButtons();
+        clock.draw(batcher, width/2, 2*height/3, width/2);
+        font.draw(batcher, world.getTargetTime().toString(), 3*width/5, width/2);
         batcher.end();
     }
 
@@ -37,16 +42,13 @@ public class ClockDropRenderer extends GameRenderer {
         turtleButton.draw(batcher);
     }
 
-    public void drawClock() {
-
-    }
-
     @Override
     public boolean touchDown(int screenX, int screenY) {
         if (bar.isMax()) game.setScreen(new MovableClockScreen(game));
         super.touchDown(screenX, screenY);
         if (trashButton.isClicked(screenX, screenY)) world.pushTrash();
         else if(turtleButton.isClicked(screenX, screenY)) world.pushTurtle();
+        clock.setTime(world.getClockTime());
         return true;
     }
 }
