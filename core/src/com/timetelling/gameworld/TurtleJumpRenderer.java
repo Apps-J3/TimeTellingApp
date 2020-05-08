c5b7d542a290cb43e7154e8c0d8d70afcd6ab993
mdari <mdariotis@charter.net>
2020-05-08 15:14:20 -0400

    maria was here pt. 2

--- core/src/com/timetelling/gameworld/TurtleJumpRenderer.java
+++ core/src/com/timetelling/gameworld/TurtleJumpRenderer.java
@@ -2,7 +2,9 @@ package com.timetelling.gameworld;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.timetelling.game.TimeTellingGame;
 import com.timetelling.gameobjects.Clock;
@@ -22,13 +24,24 @@ public class TurtleJumpRenderer extends GameRenderer {
     private TurtleJumpWorld world;
     private Skin skin;
     private Clock clock;
+    private BitmapFont font;
+    private ImgButton message;
+    private ImgButton infoMessage;
+    private Sprite background;
+    private Texture turtle;
+
 
     public TurtleJumpRenderer(GameWorld world, TimeTellingGame game) {
         super(world, game);
         this.world = (TurtleJumpWorld)world;
         bar = this.world.getBar();
         choices = this.world.getChoices();
+        background = AssetLoader.turtleJumpBackgroundSprite;
+        turtle = AssetLoader.turtle;
         buttons = new ImgButton[choices.length];
+        font = AssetLoader.font;
+        infoMessage = new ImgButton(0,height/6, width/4,height/6,"Click the corresponding time", AssetLoader.clouds,AssetLoader.font,0,0);
+        message = new ImgButton(7*width/10,height/6,width/4,height/6,"",AssetLoader.clouds,AssetLoader.font,0,0);
         clock = new Clock(this.world.getCorrectTime());
         for (int i = 0; i < buttons.length; i++) {
             buttons[i] = new ImgButton((i+1)*width/(buttons.length+2), width/3, width/(buttons.length+2), width/6, "", AssetLoader.platform, AssetLoader.font, ((width/(buttons.length+2))/5)*2,width/12);
@@ -39,9 +52,13 @@ public class TurtleJumpRenderer extends GameRenderer {
     public void render() {
         super.render();
         batcher.begin();
+        batcher.draw(background,width/13,0);
         drawButtons();
         clock.draw(batcher, width/2, 2*height/3, width/2);
+        infoMessage.draw(batcher);
+        message.draw(batcher);
         bar.draw(batcher, width/5, 9*height/10, 3*width/5, height/20);
+        batcher.draw(turtle, 17*width/40,height/10,width/6,height/5);
         batcher.end();
     }
 
@@ -60,7 +77,11 @@ public class TurtleJumpRenderer extends GameRenderer {
                 boolean answer = world.guess(choices[i]);
                 if (answer) {
                     if (bar.isMax()) game.setScreen(new TitleScreen(game));
-                } else {
+                    infoMessage = new ImgButton(0,height/6, width/4,height/6,"Click the corresponding time", AssetLoader.clouds,AssetLoader.font,0,0);
+                    message = new ImgButton(7*width/10,height/6,width/4,height/6,"Great job!",AssetLoader.clouds,AssetLoader.font,0,0);
+                }
+                else {
+                    message = new ImgButton(7*width/10,height/6,width/4,height/6,"Oops, the correct time is " + this.world.getCorrectTime().toString(),AssetLoader.clouds,AssetLoader.font,0,0);
 
                 }
                 clock.setTime(world.getCorrectTime());
