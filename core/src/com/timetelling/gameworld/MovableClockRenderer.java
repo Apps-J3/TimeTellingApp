654d4b6ed6e39c46263aa21be4562fe605e2b5f7
mdari <mdariotis@charter.net>
2020-05-09 01:31:48 -0400

    maria was here pt. 2

--- core/src/com/timetelling/gameworld/MovableClockRenderer.java
+++ core/src/com/timetelling/gameworld/MovableClockRenderer.java
@@ -3,6 +3,7 @@ package com.timetelling.gameworld;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.timetelling.game.TimeTellingGame;
+import com.timetelling.gameobjects.ImgButton;
 import com.timetelling.gameobjects.MovableClock;
 import com.timetelling.gameobjects.ProgressBar;
 import com.timetelling.gameobjects.SimpleButton;
@@ -14,18 +15,22 @@ public class MovableClockRenderer extends GameRenderer{
     private MovableClock clock;
     private BitmapFont font;
     private MovableClockWorld world;
-    private SimpleButton submitButton;
+   // private SimpleButton submitButton;
+    private ImgButton submitButton;
     private Texture turtle;
     private String message;
     private String errorMessage;
     private ProgressBar bar;
+    private Texture platform;
 
     public MovableClockRenderer(GameWorld world, TimeTellingGame game) {
         super(world, game);
         clock = new MovableClock(width/2, 2*height/3, width/2);
         font = AssetLoader.font;
-        submitButton = new SimpleButton(width/2, height/5, width/3, height/8, "Submit Time");
-        turtle = AssetLoader.turtle;
+        platform = AssetLoader.platform;
+        //submitButton = new SimpleButton(width/2, height/5, width/3, height/8, "Submit Time");
+        submitButton = new ImgButton(width/2, height/5, width/3, height/6, "Submit Time", AssetLoader.clouds, AssetLoader.font,0,0);
+        turtle = AssetLoader.turtleShadow;
         this.world = (MovableClockWorld)world;
         this.world.setClock(clock);
         message = "Set the clock to " + this.world.getTargetTime().toString();
@@ -38,7 +43,8 @@ public class MovableClockRenderer extends GameRenderer{
         super.render();
         batcher.begin();
         clock.draw(batcher);
-        batcher.draw(turtle, width/6, height/8, width/5, height/4);
+        batcher.draw(platform,width/6, height/50, width/4,width/6);
+        batcher.draw(turtle, width/7, height/8, 5*width/20, height/4);
         submitButton.draw(batcher);
         font.draw(batcher, message, width/6, 3*height/8);
         font.draw(batcher, errorMessage, width/2, 14*height/40);
